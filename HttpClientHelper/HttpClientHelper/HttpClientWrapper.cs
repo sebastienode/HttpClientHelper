df401e001bc7b143cfbfbde3e4705d5b5db40f8f
ODE <sebastienode@gmail.com>
2025-06-30 13:13:59 +0200

    Add CancellationToken and HttpCompletion

--- HttpClientHelper/HttpClientHelper/HttpClientWrapper.cs
+++ HttpClientHelper/HttpClientHelper/HttpClientWrapper.cs
@@ -9,11 +9,29 @@
             _client = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
         }
 
+        /// <summary>
+        /// Sends an HTTP request message and returns the response message asynchronously.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
         {
-            return await _client.SendAsync(request);
+            return await SendAsync(request, CancellationToken.None);
         }
 
+        public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken, HttpCompletionOption completionOption = HttpCompletionOption.ResponseHeadersRead)
+        {
+            return await _client.SendAsync(request, completionOption, cancellationToken);
+        }
+
+        /// <summary>
+        /// Creates an HTTP request message with the specified method, relative URI, and optional parameters.
+        /// </summary>
+        /// <param name="method"></param>
+        /// <param name="relativeUri"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
         public HttpRequestMessage CreateRequest(HttpMethod method, string relativeUri, IToDictionnary? parameters)
         {
             if (_client.BaseAddress == null)
